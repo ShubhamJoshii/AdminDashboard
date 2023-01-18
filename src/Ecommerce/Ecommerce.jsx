5a3ee2b8eef97d8814b0d8e63aa2702128a33690
ShubhamJoshii <shubhamjoshii676@gmail.com>
2023-01-18 16:33:17 +0530

    Ecommerce Added

--- src/Ecommerce/Ecommerce.jsx
+++ src/Ecommerce/Ecommerce.jsx
@@ -4,6 +4,9 @@ import { MdSupervisedUserCircle } from "react-icons/md";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BiBarChart } from "react-icons/bi";
 import { FaSyncAlt } from "react-icons/fa";
+import { MdOutlineSecurity } from "react-icons/md";
+import { BiTask } from "react-icons/bi";
+import { RiProfileLine} from "react-icons/ri";
 const Ecommerce = ({ themeColor }) => {
   return (
     <div className="Ecommerce">
@@ -47,6 +50,75 @@ const Ecommerce = ({ themeColor }) => {
             </h2>
             <h3>Customers</h3>
           </div>
+        </div>
+      </div>
+      <div id="recentTranscation">
+        <h2>Recent Transactions</h2>
+        <div className="profileTopicEcomm">
+          <div id="profileContainer">
+            <RiProfileLine
+              style={{ background: "#E5FAFB", color: "#03C9D7" }}
+            />
+            <div>
+              <h3>UPI Transfer</h3>
+              <p>Money Added</p>
+            </div>
+          </div>
+          <p id="priceTranscation" style={{ color: "#1DA348" }}>
+            + ₹34,000
+          </p>
+        </div>
+        <div className="profileTopicEcomm">
+          <div id="profileContainer">
+            <MdOutlineSecurity
+              style={{ background: "#EBFAF2", color: "#00C292" }}
+            />
+            <div>
+              <h3>Wallet</h3>
+              <p>Bill Payment</p>
+            </div>
+          </div>
+          <p id="priceTranscation" style={{ color: "red" }}>
+            - ₹3,250
+          </p>
+        </div>
+        <div className="profileTopicEcomm">
+          <div id="profileContainer">
+            <RiProfileLine
+              style={{ background: "#E5FAFB", color: "#03C9D7" }}
+            />
+            <div>
+              <h3>Credit Card</h3>
+              <p>Money reversed</p>
+            </div>
+          </div>
+          <p id="priceTranscation" style={{ color: "#1DA348" }}>
+            + ₹20,000
+          </p>
+        </div>
+        <div className="profileTopicEcomm">
+          <div id="profileContainer">
+            <BiTask style={{ background: "#FEC90F", color: "#FFEECA" }} />
+            <div>
+              <h3>Bank Transfer</h3>
+              <p>Money Added</p>
+            </div>
+          </div>
+          <p id="priceTranscation" style={{ color: "#1DA348" }}>
+            + ₹14,000
+          </p>
+        </div>
+        <div className="profileTopicEcomm">
+          <div id="profileContainer">
+            <BiTask style={{ background: "#FFF4E5", color: "#E46A76" }} />
+            <div>
+              <h3>Refund</h3>
+              <p>Payment Sent</p>
+            </div>
+          </div>
+          <p id="priceTranscation" style={{ color: "red" }}>
+            - ₹4,000
+          </p>
         </div>
       </div>
     </div>
